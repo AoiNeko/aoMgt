7040a860e4529e933e4040a53acb444dbde0d17f
aoineko <liangli171@gmail.com>
2018-05-04 01:05:26 +0800

    import css。  ant calendar

--- src/mgt/Mgt.js
+++ src/mgt/Mgt.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import AoCalendar from './component/AoCalendar'
+
+
+function onPanelChange(value, mode) {
+    console.log(value, mode);
+}
 
 class Mgt extends Component {
 
      render() {
-        return (<AoCalendar/>)
+        return (<AoCalendar />)
      }
 }
+
+
 export default Mgt;
